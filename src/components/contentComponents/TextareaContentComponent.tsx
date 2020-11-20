e7d46d5b25b1d4d8ec71e106cd1d51d27a20ee60
Gareth Brown <gareth.brown@bjss.com>
2020-11-20 15:51:16 +0000

    Add validation for TextareaContentComponent

--- src/components/contentComponents/TextareaContentComponent.tsx
+++ src/components/contentComponents/TextareaContentComponent.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { TextareaContent } from '../../data/formInstructionsInterface';
-import { updateValue } from '../../state/actions';
+import { updateIsValid, updateValue } from '../../state/actions';
+import { validate } from '../../state/utilities';
 import { SharedContentProps } from '../contentInterfaces';
-import { Label, Textarea } from '../sharedComponents';
+import { Label, Textarea, ValidationMessage } from '../sharedComponents';
 
 interface TextareaContentProps extends SharedContentProps {
   textareaContent: TextareaContent;
@@ -15,6 +16,16 @@ export const TextareaContentComponent = ({ textareaContent, dispatch, contentIte
     event.preventDefault();
     event.stopPropagation();
     dispatch(updateValue(sectionId, contentItemState.id, event.target.value));
+    checkValidation(event.target.value);
+  };
+
+  const handleBlur = (event: any) => { 
+    checkValidation(event.target.value);
+  };
+
+  const checkValidation = (value: string) => {
+    const isValid = validate(value as string, textareaContent.metadata.required);
+    dispatch(updateIsValid(sectionId, contentItemState.id, isValid));
   };
   
   return (
@@ -25,8 +36,10 @@ export const TextareaContentComponent = ({ textareaContent, dispatch, contentIte
         name={textareaContent.id}
         placeholder={textareaContent.metadata?.placeholder}
         onChange={handleChange}
+        onBlur={handleBlur}
         value={contentItemState.value as string}
       />
+      {!contentItemState.isValid && <ValidationMessage>* Please enter valid text</ValidationMessage>}
     </>
   );
 };
