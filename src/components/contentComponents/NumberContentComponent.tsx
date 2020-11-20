6b988ca486e611d58fb139a02010f7ed8ff39cd2
Gareth Brown <gareth.brown@bjss.com>
2020-11-20 13:33:57 +0000

    Use state for NumberContentComponent

--- src/components/contentComponents/NumberContentComponent.tsx
+++ src/components/contentComponents/NumberContentComponent.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NumberContent } from '../../data/formInstructionsInterface';
+import { updateValue } from '../../state/actions';
 import { SharedContentProps } from '../contentInterfaces';
 import { Label, Input } from '../sharedComponents';
 
@@ -7,13 +8,25 @@ interface NumberContentProps extends SharedContentProps {
   numberContent: NumberContent;
 }
 
-export const NumberContentComponent = ({ numberContent, dispatch }: NumberContentProps) => {
+export const NumberContentComponent = ({ numberContent, dispatch, contentItemState, sectionId }: NumberContentProps) => {
   if (!numberContent) return null;
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    event.stopPropagation();
+    dispatch(updateValue(sectionId, contentItemState.id, event.target.value));
+  };
 
   return (
     <>
       <Label htmlFor={numberContent.id}>{numberContent.question_text}</Label>
-      <Input type="number" id={numberContent.id} name={numberContent.id} />
+      <Input
+        type="number"
+        id={numberContent.id}
+        name={numberContent.id}
+        onChange={handleChange}
+        value={contentItemState.value as number}
+      />
     </>
   );
 };
