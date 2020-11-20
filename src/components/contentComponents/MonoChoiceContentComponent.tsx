b201267648ef2255f169367cd317567a1082bd16
Gareth Brown <gareth.brown@bjss.com>
2020-11-20 10:33:14 +0000

    Use state for MonoChoiceContentComponent value

--- src/components/contentComponents/MonoChoiceContentComponent.tsx
+++ src/components/contentComponents/MonoChoiceContentComponent.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { MonoChoiceContent } from '../../data/formInstructionsInterface';
+import { updateValue } from '../../state/actions';
 import { SharedContentProps } from '../contentInterfaces';
 import { Label, Select } from '../sharedComponents';
 
@@ -7,13 +8,22 @@ interface MonoChoiceContentProps extends SharedContentProps {
   monoChoiceContent: MonoChoiceContent;
 }
 
-export const MonoChoiceContentComponent = ({ monoChoiceContent, dispatch }: MonoChoiceContentProps) => {
+export const MonoChoiceContentComponent = ({ monoChoiceContent, dispatch, contentItemState, sectionId }: MonoChoiceContentProps) => {
   if (!monoChoiceContent) return null;
+
+  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+     console.log('value: ', event.target.value);
+    dispatch(updateValue(sectionId, contentItemState.id, event.target.value));
+  };
 
   return (
     <>
       <Label htmlFor={monoChoiceContent.id}>{monoChoiceContent.question_text}</Label>
-      <Select id={monoChoiceContent.id} name={monoChoiceContent.id}>
+      <Select
+        id={monoChoiceContent.id} name={monoChoiceContent.id}
+        value={contentItemState.value as string}
+        onChange={handleChange}
+      >
         {monoChoiceContent.metadata?.options.map(({value, label}) => (
           <option key={value} value={value}>
             {label}
