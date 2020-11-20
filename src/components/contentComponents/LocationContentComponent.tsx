c852394b9ebacc752a94fc5468786b931006d19f
Gareth Brown <gareth.brown@bjss.com>
2020-11-20 10:25:13 +0000

    Use state for location component

--- src/components/contentComponents/LocationContentComponent.tsx
+++ src/components/contentComponents/LocationContentComponent.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { LocationContent } from '../../data/formInstructionsInterface';
+import { updateValue } from '../../state/actions';
 import { SharedContentProps } from '../contentInterfaces';
 import { Label, Input } from '../sharedComponents';
 
@@ -7,8 +8,12 @@ interface LocationContentProps extends SharedContentProps {
   locationContent: LocationContent;
 }
 
-export const LocationContentComponent = ({ locationContent, dispatch }: LocationContentProps) => {
+export const LocationContentComponent = ({ locationContent, dispatch, contentItemState, sectionId }: LocationContentProps) => {
   if (!locationContent) return null;
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(updateValue(sectionId, contentItemState.id, event.target.value));
+  };
 
   return (
     <>
@@ -18,6 +23,8 @@ export const LocationContentComponent = ({ locationContent, dispatch }: Location
         id={locationContent.id}
         name={locationContent.id}
         placeholder={locationContent.metadata?.placeholder}
+        onChange={handleChange}
+        value={contentItemState.value as string}
       />
     </>
   );
