ec2dbb87444047012bda6d2e8f579d28fab4c80a
Gareth Brown <gareth.brown@bjss.com>
2020-11-20 13:13:51 +0000

    Use state for UrlContentComponent

--- src/components/contentComponents/UrlContentComponent.tsx
+++ src/components/contentComponents/UrlContentComponent.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { UrlContent } from '../../data/formInstructionsInterface';
+import { updateValue } from '../../state/actions';
 import { SharedContentProps } from '../contentInterfaces';
 import { Label, Input } from '../sharedComponents';
 
@@ -7,13 +8,23 @@ interface UrlContentProps extends SharedContentProps {
   urlContent: UrlContent;
 }
 
-export const UrlContentComponent = ({ urlContent, dispatch }: UrlContentProps) => {
+export const UrlContentComponent = ({ urlContent, dispatch, contentItemState, sectionId }: UrlContentProps) => {
   if (!urlContent) return null;
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(updateValue(sectionId, contentItemState.id, event.target.value));
+  };
 
   return (
     <>
       <Label htmlFor={urlContent.id}>{urlContent.question_text}</Label>
-      <Input type="text" id={urlContent.id} name={urlContent.id} />
+      <Input
+        type="text"
+        id={urlContent.id}
+        name={urlContent.id}
+        onChange={handleChange}
+        value={contentItemState.value as string}
+      />
     </>
   );
 };
